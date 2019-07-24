5dd91f42b6df54e2365bb08c779fa48bd948f8c4
Andrew Cunningham <andrew@Andrews-MacBook-Pro.local>
2019-07-24 16:10:29 -0700

    timer stops once copmplete

--- memory-game/src/scorecontainer.js
+++ memory-game/src/scorecontainer.js
@@ -5,7 +5,7 @@ function ScoreContainer(props) {
   return (
     <div className="score-container">
     <h1>Score Container </h1>
-    <Score score={props.score} shuffle={props.shuffle}/>
+    <Score score={props.score} shuffle={props.shuffle} matchedPairs={props.matchedPairs}/>
     </div>
   );
 }
