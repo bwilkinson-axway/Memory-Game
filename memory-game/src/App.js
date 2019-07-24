a26f018e931985492342b50713ee2e5b6350b1d2
Andrew Cunningham <andrew@Andrews-MacBook-Pro.local>
2019-07-24 10:15:38 -0700

    added state and random order to state

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,26 +1,130 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import uuid from 'uuid';
+import { throws } from 'assert';
 
-function App() {
+class App extends Component {
+  state = {
+    cards: [
+      {
+        id: uuid.v4(),
+        picture: 'https://acdn.list25.com/wp-content/uploads/2015/06/Muscular.jpg',
+        matched: false,
+        itemId:1
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://acdn.list25.com/wp-content/uploads/2015/06/Muscular.jpg',
+        matched: false,
+        itemId:1
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:2
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:2
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:3
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:3
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:4
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:4
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:5
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:5
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:6
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:6
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:7
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:7
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:8
+      },
+      {
+        id: uuid.v4(),
+        picture: 'https://www.publicdomainpictures.net/pictures/40000/nahled/question-mark.jpg',
+        matched: false,
+        itemId:8
+      }
+  
+    ]
+  }
+
+   shuffleState = this.state.cards.sort(function(){
+    console.log('im running')
+    return 0.5 - Math.random()
+  }) 
+  
+
+
+  render(){
+    let shuffleState;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {console.log(this.state.cards)}
+       
+     
       </header>
     </div>
   );
-}
+}}
 
 export default App;
