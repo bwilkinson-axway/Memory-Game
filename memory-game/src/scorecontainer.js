bd7020e2cf0b45b8b9bcbc7599cbca27ff8ed1e5
bradtxufl <brad.w@utexas.edu>
2019-07-24 16:44:00 -0700

    looks orange

--- memory-game/src/scorecontainer.js
+++ memory-game/src/scorecontainer.js
@@ -4,7 +4,7 @@ import Score from './score'
 function ScoreContainer(props) {
   return (
     <div className="score-container">
-    <img src='http://s3.amazonaws.com/production.mediajoint.prx.org/public/piece_images/476488/Match_Game_orange_logo_small.png' />
+    <img id='logo' src='http://s3.amazonaws.com/production.mediajoint.prx.org/public/piece_images/476488/Match_Game_orange_logo_small.png' />
     <Score score={props.score} shuffle={props.shuffle} matchedPairs={props.matchedPairs} />
     </div>
   );
