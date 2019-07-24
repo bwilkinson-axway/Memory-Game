72584d6d29090a5fba56b292ead941492bdb5335
bradtxufl <brad.w@utexas.edu>
2019-07-24 11:58:58 -0700

    hmm

--- memory-game/src/score.js
+++ memory-game/src/score.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-function Score() {
-  return (
-    <div className="score">
-    <h1>Score: 10000</h1>
-    <h1>Timer: 45seconds</h1>
-    <button>New Game</button>
-    </div>
-  );
+class Score extends React.Component {
+
+  render() {
+
+    return (
+      <div className="score">
+      <h1>Score: 10000</h1>
+      <h1>Timer: 0 seconds</h1>
+      <button>New Game</button>
+      </div>
+    );
+  }
 }
 
 export default Score;
